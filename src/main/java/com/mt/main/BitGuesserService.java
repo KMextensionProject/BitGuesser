730efb755c580ff5ae458fdffbd4ad82b8faabfa
Martin Krajcovic <mkrajcovic@gratex.com>
2023-07-26 17:14:45 +0200

    BitGuesserService - added comments + added default StandardOutputNotification implementation

--- src/main/java/com/mt/main/BitGuesserService.java
+++ src/main/java/com/mt/main/BitGuesserService.java
@@ -4,6 +4,7 @@ import static com.mt.config.ConfigurationKey.NOTIFICATION_RECIPIENT_EMAIL;
 import static com.mt.config.ConfigurationKey.NOTIFICATION_RECIPIENT_OTHER_CONTACT;
 import static com.mt.config.ConfigurationKey.NOTIFICATION_RECIPIENT_PHONE;
 import static java.lang.Runtime.getRuntime;
+import static java.util.Objects.isNull;
 import static java.util.concurrent.CompletableFuture.runAsync;
 import static java.util.concurrent.Executors.newFixedThreadPool;
 
@@ -12,6 +13,7 @@ import java.security.Security;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
@@ -26,6 +28,18 @@ import com.mt.notification.Notification;
 import com.mt.notification.Recipient;
 
 /**
+ * This class represents the main operational interface of this application.<br>
+ * It provides an easy way to generate amounts of Bitcoin wallets and perform
+ * their processing via single method that composes the necessary operations in
+ * accordance with the business case of this application. Internal units of the
+ * processing are configured by the
+ * {@link com.mt.config.ApplicationConfiguration} object.
+ *
+ * <p>
+ * By itself, this class manages all the necessary resources needed during the
+ * whole life of the application. These resources are released automatically
+ * before termination as part of the JVM shutdown hook.
+ * </p>
  *
  * @author mkrajcovic
  */
@@ -64,12 +78,14 @@ public class BitGuesserService {
 
 	private void releaseResources() {
 		System.out.println("Shutting down the task executor");
-		taskProcessor.shutdown(); // do not accept any future tasks
 		try {
-			while (true) {
-				if (taskProcessor.awaitTermination(5, TimeUnit.SECONDS)) {
-					System.out.println("All running tasks have been executed successfully");
-					break;
+			if (!isNull(taskProcessor)) {
+				taskProcessor.shutdown(); // do not accept any future tasks
+				while (true) {
+					if (taskProcessor.awaitTermination(5, TimeUnit.SECONDS)) {
+						System.out.println("All running tasks have been executed successfully");
+						break;
+					}
 				}
 			}
 			System.out.println("Closing database connection");
@@ -82,9 +98,12 @@ public class BitGuesserService {
 	}
 
 	/**
+	 * Creates the required amount of Bitcoin wallets.<br>
+	 * <b><i>Note:</i></b> The generation is CPU intensive
+	 * and time complexity is O(n).
 	 *
-	 * @param howMany
-	 * @return
+	 * @param howMany - Bitcoin wallets to generate.
+	 * @return list of generated Bitcoin wallets
 	 */
 	public List<Wallet> generateWallets(int howMany) {
 		final List<Wallet> wallets = new ArrayList<>(howMany);
@@ -95,19 +114,32 @@ public class BitGuesserService {
 	}
 
 	/**
+	 * Delegates the synchronous version of wallet processing to the underlying
+	 * single-threaded task executor, so the {@link #generateWallets(int)} can
+	 * be used to generate continuously more wallets without blocking while
+	 * waiting on database operations to complete. <br>
+	 * See {@link #processWallets(List)} for more behavior details.
 	 *
-	 * @param wallets
+	 * @param wallets - to process
 	 */
-	public void processWalletsAsync(List<Wallet> wallets) {
+	public CompletableFuture<Void> processWalletsAsync(List<Wallet> wallets) {
 		if (taskProcessor == null) {
 			taskProcessor = newFixedThreadPool(1);
 		}
-		runAsync(() -> processWallets(wallets), taskProcessor);
+		return runAsync(() -> processWallets(wallets), taskProcessor);
 	}
 
 	/**
+	 * Queries the database for a match on provided wallet addresses. If there
+	 * is a match, then the private keys will be saved to corresponding
+	 * addresses along with notifying the client to all defined
+	 * {@link com.mt.notification.Notification} implementations.
+	 * <p>
+	 * All the wallets passed into this method will be saved to database if
+	 * configured via {@code db.table.save_wallet} properties.
+	 * </p>
 	 *
-	 * @param wallets
+	 * @param wallets - to find a match for
 	 */
 	public void processWallets(List<Wallet> wallets) {
 		db.saveWallets(wallets);
@@ -135,6 +167,5 @@ public class BitGuesserService {
 				ioex.printStackTrace();
 			}
 		}
-		System.out.println("notifications have been sent");
 	}
 }
---
+++ src/main/java/com/mt/notification/StandardOutputNotification.java
@@ -0,0 +1,21 @@
+package com.mt.notification;
+
+import java.io.IOException;
+
+/**
+ * Default Notification implementation.
+ *
+ * @author mkrajcovic
+ *
+ */
+public class StandardOutputNotification implements Notification {
+
+	/**
+	 * Prints the message body to the standard output.
+	 */
+	@Override
+	public void sendNotification(Message message, Recipient recipient) throws IOException {
+		System.out.println("Console Notification: " + message.getSubject() + " - " + message.getBody());
+	}
+
+}
